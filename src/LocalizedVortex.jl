11d5cde79d5452b51fa2f44d1174f960164a09c5
mleprovost <mathow@g.ucla.edu>
2022-08-04 13:51:20 -0700

    Removing unneccary dependencies

--- src/LocalizedVortex.jl
+++ src/LocalizedVortex.jl
@@ -2,12 +2,9 @@ module LocalizedVortex
 
 using TransportBasedInference
 using LinearAlgebra
-using LoopVectorization
 using PotentialFlow
 using ProgressMeter
 
-include("tools.jl")
-include("aggregation.jl")
 include("vortextools.jl")
 include("state_equation.jl")
 include("pressure.jl")
